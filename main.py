fb7f79d0f40d2a8894b16cfb4c23524dac6f3893
litzleo <49692302+litzleo@users.noreply.github.com>
2024-07-15 18:55:38 +0200

    Aggiunti filtri agli input

--- main.py
+++ main.py
@@ -49,16 +49,76 @@ def visitPage(page, instructions):
             time.sleep(waitTime/1000)
     driver.quit()
 
+def isValidPage(pageId, limit):
+    try:
+        id = int(pageId)
+        return id>0 and id<=limit
+    except:
+        return False
+    
+def isValidUserQuantityList(comm, u):
+    s = ''
+    if comm == '':
+        return False
+    users = list(u)
+    for i in range(len(users)):
+        users[i] = users[i].lower()
+
+    state = 'user'
+    num = False
+    user = ''
+    quantity = 0
+    for c in comm.lower():
+        ascii = ord(c)
+        if state == 'user':
+            if ascii >= ord('a') and ascii <= ord('z'):
+                user += c
+            elif c==':':
+                if user not in users: 
+                    return False
+                users.remove(user)
+                s += user+':'
+                user = ''
+                state = 'quantity'
+                num = False
+            elif c==' ':
+                pass
+            else:
+                return False
+        elif state == 'quantity':
+            if ascii >= ord('0') and ascii <= ord('9'):
+                quantity *= 10
+                quantity += int(ascii)
+                s += c
+                num = True
+            elif c==' ':
+                if num:
+                    state = 'user'
+                    s += ' '
+            elif c!='\0':
+                return False
+    return s
+            
 
 #Si richiede all'utente di selezionare una pagina tra quelle disponibili
 
 pages = aggregator.getPages()
 
+error_message = ' (input invalido, riprova)'
+
 print('Siti disponibili:')
 for ind in range(len(pages)):
     print(str(ind+1)+' - '+pages[ind])
 
-pageInd = input('\nScegli un sito: ')
+mx = '\nScegli un sito (specificandone solo il numero)'
+pageInd = 0
+firstLoop = True
+while(not isValidPage(pageInd, len(pages))):
+    pageInd = input(mx+': ')
+    if firstLoop:
+        mx += error_message
+    firstLoop = False
+
 page = pages[int(pageInd)-1]
 
 #Permette di scegliere quante istanze di ogni tipo di utente virtuale processare
@@ -70,11 +130,22 @@ print("\nTipi di utenti: ")
 for u in users:
     print(' - '+u)
 
-comm = input('\nSeleziona quantità e tipi: ')
+comm = ''
+mx = '\nSeleziona tipi e quantità (usa il formato "tipoutente:quantità tipoutente:quantità")'
+firstLoop = True
+invalidInput = True
+while(invalidInput):
+    comm = input(mx+': ')
+    comm = isValidUserQuantityList(comm.rstrip(), users)
+    if(comm != False):
+        invalidInput = False
+    if firstLoop:
+        mx += error_message
+    firstLoop = False
+    
+quantities = comm.split()
 
 #vengono generati dei thread ognuno dei quali imita il comportamento di un utente
-
-quantities = comm.split()
 
 for q in quantities:
     userType, amount = q.split(':')
